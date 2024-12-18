5fbadb8df0fc70420b4b7587d40ea20ca9451506
nafimbrk <nafimubarok2005@gmail.com>
2024-12-18 20:50:22 +0700

    components

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Student @response="showData" studentName="andi">ini adalah nilai slot dari parent</Student> <br>
+  <Student @response="showData" studentName="budi"></Student> <br>
+  <Student @response="showData" studentName="ani"></Student>
+
+  <p>
+    student yg dipilih adalah: {{ selectedStudent }}
+  </p>
+</template>
+
+<script>
+import Student from './components/Student.vue';
+
+export default {
+  components: {
+    Student
+  },
+  data() {
+    return {
+      selectedStudent: ''
+    }
+  },
+  methods: {
+    showData(whatever) {
+      this.selectedStudent = whatever
+    }
+  }
+}
+</script>
---
+++ src/components/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <slot>ini adalah slot default karena parent component tidak memberikan data</slot> <br>
+    ini adalah data dari siswa yg bernama {{ studentName }} <button @click="showData">show detail</button> <br>
+</template>
+
+<script>
+export default {
+    props: {
+        studentName: String
+    },
+    emits: ['response'],
+    methods: {
+        showData() {
+            this.$emit('response', this.studentName)
+        }
+    }
+}
+</script>
